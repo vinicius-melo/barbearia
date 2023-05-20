714b2a094c47a2de8f9d9c0e195b62a96c9b8235
Heloisa Barbosa <heloisabbarbosasilva@gmail.com>
2023-05-20 17:23:17 -0300

    helo 20/05/23 17:22

---
+++ app/src/main/java/com/barbearia/Relatorio.java
@@ -0,0 +1,14 @@
+package com.barbearia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Relatorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio);
+    }
+}
